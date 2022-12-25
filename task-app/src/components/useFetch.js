edeee9762e9d663fc0e0980dbb0c7b8b4c2c6dc5
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-12-25 21:24:09 +0000

    created UseFetch Hook

---
+++ task-app/src/components/useFetch.js
@@ -0,0 +1,15 @@
+// need api route
+// take a REST Method
+
+export const UseFetch = (api, method, data)=>{
+    const requestOptions = {
+        method:method,
+        mode:'cors',
+       headers:{"Content-Type": "application/json"},
+        body: JSON.stringify(data)
+     }
+console.log(data)
+
+     fetch(api, requestOptions)
+
+}
