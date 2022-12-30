a76a3559afa952e1d0c1309bdee61344e5571672
zach7815 <53745970+zach7815@users.noreply.github.com>
2022-12-30 21:38:00 +0000

    changed how cors is implemented

--- backend/server.mjs
+++ backend/server.mjs
@@ -15,13 +15,6 @@ app.use(urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
 app.use(cors);
-app.use(function (req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', "http://localhost:3000/");
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-    });
 
 const PORT = process.env.PORT || 8000;
 
