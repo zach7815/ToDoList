14868de7680b43f29581f04d39abd317fa39d61d
zach7815 <53745970+zach7815@users.noreply.github.com>
2023-01-01 18:19:32 +0000

    changes to mongo connect in order to enable hosting

--- backend/server.mjs
+++ backend/server.mjs
@@ -1,43 +1,67 @@
 import * as dotenv from 'dotenv';
 dotenv.config();
 import express, { urlencoded } from "express";
-import cors from 'cors';
-import mongoose from "mongoose";
-import toDo from "./toDo.mjs";
-
-const connectionString= process.env.CONNECTION_STRING;
-
+import mongoose, {Schema} from "mongoose";
 
 
 const app = express();
+const connectionString= process.env.CONNECTION_STRING;
+
+mongoose.set('strictQuery', false);
+
+const toDoSchema=  new Schema({
+    id:Number,
+    text:String,
+    complete:Boolean
+});
+
 
 app.use(urlencoded({extended:true}));
 app.use(express.static('public'));
 app.use(express.json({limit:'1mb'}));
-app.use(cors);
+
+
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', "https://todo-app-frontend-obe0.onrender.com" );
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+    });
+
+
+
 
 const PORT = process.env.PORT || 8000;
 
 
-main().catch(err => console.log(err));
+main();
+
 async function main() {
-    await mongoose.connect(connectionString,
-    ()=>{ console.log("MongoDB connected");},
-    (e)=>{console.log({error:e.message})}
-    );
+    try{
+        await mongoose.connect(connectionString);
+         await mongoose.model("toDo", toDoSchema);
+
+
+    }
+
+    catch(error){
+        console.log(error);
+    }
   };
-
-
+    main();
+    const toDo= await mongoose.model("toDo", toDoSchema);
 
 
 
 app.get("/", (req,res)=>{
-    res.send("This is a backend route please go localHost: 3000");
+    res.send("This is a backend route please go to the front end");
 })
 
 app.get("/api/loadtoDos",  async (req,res)=>{
     try{
        const toDos = await toDo.find();
+       console.log(toDos);
         const toDoList= JSON.stringify(toDos);
        res.send(toDoList);
     }
